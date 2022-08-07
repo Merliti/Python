0bbed6c9693a84255435bd5e082a9429f47f1781
Merliti <merliti@hotmail.com>
2022-08-07 14:48:01 +0300

    Intro to python files from 7th of august lesson

---
+++ numeric.py
@@ -0,0 +1,31 @@
+# int, float, complex
+
+"""
+integers:
+    -whole number
+    - + or -
+    - unlimited lenght
+
+floats
+    -number with decimal
+    - + or -
+    -scientific number 'e' can be used to denote the power of10
+Complex
+    complex numbers are written with the imaginary part 'j'
+
+type conversion/casting
+    - converting from a data type to another
+
+
+"""
+age = 10
+
+print(type(age))
+bal = 123.3
+e = 123e45
+print(type(e))
+comp = 3j + 8
+
+balint = int(bal)
+print(type(comp))
+print(type(balint))
